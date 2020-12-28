9c803ff431605b7dcb6742f716f6b7528bb0e65f
NetLSS <38928282+NetLSS@users.noreply.github.com>
2020-12-28 22:59:45 +0900

    chore: tmp

--- _posts/_code_tmp.py
+++ _posts/_code_tmp.py
@@ -1,17 +1,6 @@
-start = input()
-col = ord(start[0]) - ord('a') + 1 # 빼는 순서!
-row = int(start[1])
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return n * factorial_recursive(n - 1)
 
-mov_row = [-2,-2,-1,1,2, 2, 1,-1]
-mov_col = [-1, 1,2 ,2,1,-1,-2,-2]
-cnt = 0
-
-for mov_type in range(len(mov_col)):
-    final_row = row + mov_row[mov_type]
-    final_col = col + mov_col[mov_type]
-    if final_row < 1 or final_row > 8 or final_col < 1 or final_col > 8:
-        continue
-    cnt+=1
-    
-print(cnt)
-     
+print(factorial_recursive(5))
