1f260f4c3e12517285d84149966984b8a5822064
NetLSS <38928282+NetLSS@users.noreply.github.com>
2020-12-29 23:20:44 +0900

    post: dfs bfs 알고리즘

--- _posts/_code_tmp.py
+++ _posts/_code_tmp.py
@@ -1,6 +1,38 @@
-def factorial_recursive(n):
-    if n <= 1:
-        return 1
-    return n * factorial_recursive(n - 1)
+from collections import deque
 
-print(factorial_recursive(5))
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+
+    # 현재 노드 방문 처리
+    visited[start] = True
+
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+
+        # 해당 원소와 인접한, 방문x인 원소 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보 리스트
+visited = [False] * 9
+
+bfs(graph, 1, visited)
